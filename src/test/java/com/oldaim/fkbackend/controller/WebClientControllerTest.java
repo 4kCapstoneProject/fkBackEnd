892a887b79c81e85c3883cc79d1a11dc530c70d4
Kimdongkyun <dgk0911@gmail.com>
2022-04-20 16:09:01 +0900

    feature#6 API 문서 자동작성 및 Controller test 작성
    - Spring Rest Docs 를 활용한 API 문서 targetInfoControllerTest 작성 완료

---
+++ src/test/java/com/oldaim/fkbackend/controller/WebClientControllerTest.java
@@ -0,0 +1,12 @@
+package com.oldaim.fkbackend.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WebClientControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
