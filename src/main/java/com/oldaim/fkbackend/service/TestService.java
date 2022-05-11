7c1696eb209420eb92dc0f6f173753f079eb7d4c
Kimdongkyun <dgk0911@gmail.com>
2022-05-11 12:20:25 +0900

    test Webhook
    -webhook 서버스 테스트용 커밋

--- src/main/java/com/oldaim/fkbackend/service/TestService.java
+++ src/main/java/com/oldaim/fkbackend/service/TestService.java
@@ -1,7 +1,12 @@
 package com.oldaim.fkbackend.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TestService {
+
+    private final TargetInfoService targetInfoService;
 }
