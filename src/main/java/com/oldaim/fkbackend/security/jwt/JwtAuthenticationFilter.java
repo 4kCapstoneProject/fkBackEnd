7fe046d157d52cfeae5e5a7d9f332abeb1db3992
Kimdongkyun <dgk0911@gmail.com>
2022-05-11 15:36:54 +0900

    Update JwtAuthenticationFilter.java
    test

--- src/main/java/com/oldaim/fkbackend/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/oldaim/fkbackend/security/jwt/JwtAuthenticationFilter.java
@@ -26,6 +26,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("필터 동작전");
+        log.info(request.getContextPath());
         if(!isAcceptedPath(request.getRequestURI())) {
             String token = jwtAuthenticProvider.resolveToken(request);
 
