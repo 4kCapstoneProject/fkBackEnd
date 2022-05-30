4fbebb9b40c21e240b9e895e39fc125134696822
Kimdongkyun <dgk0911@gmail.com>
2022-05-30 15:38:00 +0900

    Update WebClientService.java

--- src/main/java/com/oldaim/fkbackend/service/WebClientService.java
+++ src/main/java/com/oldaim/fkbackend/service/WebClientService.java
@@ -22,7 +22,7 @@ import java.net.URI;
 @Slf4j
 public class WebClientService {
 
-    private final String MODEL_SERVER_URL = "http://26d6-34-80-138-53.ngrok.io";
+    private final String MODEL_SERVER_URL = "http://748f-34-80-138-53.ngrok.io";
     private final URI uri = URI.create(MODEL_SERVER_URL);
     private final WebClient client = WebClient.create(String.valueOf(uri));
     private final ImageService imageService;
